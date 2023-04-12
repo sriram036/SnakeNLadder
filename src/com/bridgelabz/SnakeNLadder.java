066778da168824616059f4b5839281350afb0574
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-04-13 00:01:51 +0530

    added: play till 100

--- src/com/bridgelabz/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeNLadder.java
@@ -13,22 +13,34 @@ public class SnakeNLadder {
     public static void main(String[] args) {
         SnakeNLadder s = new SnakeNLadder();
         int playerPosition = s.STARTPOSITION;
-        int dieRollNum = s.dieRoll();
-        System.out.println("The Dice Number is "+dieRollNum);
-        playerPosition = dieRollNum;
-        int option = s.checkOption();
-        System.out.println("The option is "+option);
-        switch (option){
-            case 1: {
-                playerPosition = playerPosition;
-            }break;
-            case 2: {
-                playerPosition = playerPosition + dieRollNum;
-            }break;
-            case 3: {
-                playerPosition = playerPosition - dieRollNum;
-            }break;
+        while(playerPosition < 100){
+            int dieRollNum = s.dieRoll();
+            System.out.println("The Dice Number is " + dieRollNum);
+            playerPosition =playerPosition + dieRollNum;
+            int option = s.checkOption();
+            System.out.println("The option is " + option);
+            switch (option) {
+                case 1: {
+                    playerPosition = playerPosition;
+                }
+                break;
+                case 2: {
+                    playerPosition = playerPosition + dieRollNum;
+                }
+                break;
+                case 3: {
+                    playerPosition = playerPosition - dieRollNum;
+                }
+                break;
+            }
+            System.out.println("The player position is " + playerPosition);
+            if(playerPosition < 0){
+                playerPosition = s.STARTPOSITION;
+            }
         }
         System.out.println("The player position is "+ playerPosition);
+        if (playerPosition >= 100) {
+            System.out.println("The player is reached "+playerPosition+" and win the game.");
+        }
     }
 }
